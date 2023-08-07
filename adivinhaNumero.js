46880238423ed34914bf790d10a8c9a88a476c46
Matheus Sales <mhss2904@gmail.com>
2023-08-07 15:19:24 -0300

    implementacao de novas funcionalidades!

--- adivinhaNumero.js
+++ adivinhaNumero.js
@@ -1,7 +1,8 @@
 var numTentativas = 1;
+let numSecreto;
 
 function adivinhar(){
-    const numSecreto = 79;
+
     const res = document.getElementById('res');
     const numInput = document.getElementById('numero');
     const num = Number(numInput.value);
@@ -14,7 +15,13 @@ function adivinhar(){
         numTentativas++;
     } else if (num == numSecreto) {
         res.innerHTML = `Parabéns! Você encontrou o número secreto!<br>Número de tentativas: ${numTentativas}`;
+        numTentativas = 0
     }
-    
-    
+}
+
+function gerarNúmeroAleatorio(){
+    numSecreto = Math.floor((Math.random() * 100));
+    numTentativas = 1;
+
+    return numSecreto;
 }
